9787db50fc74e632d0655b0d83b89c5540a3d12c
Cristian Capdevila <ccapdevi@utk.edu>
2015-04-13 15:19:45 -0400

    dependencies.. yay

--- project/Glove4jBuild.scala
+++ project/Glove4jBuild.scala
@@ -13,10 +13,10 @@ object Glove4jBuild extends Build {
       scalaVersion := "2.10.4",
 
       libraryDependencies ++= Seq(
-        "com.github.scopt" %% "scopt" % "3.2.0",
-        "org.nd4j" % "nd4j-jcublas-6.0" % "0.0.3.5.5.3-SNAPSHOT",
-        "org.nd4j" % "nd4j-api" % "0.0.3.5.5.3-SNAPSHOT",
-        "org.deeplearning4j" % "deeplearning4j-nlp" % "0.0.3.3.3.alpha1-SNAPSHOT"
+        "com.github.scopt" %% "scopt" % "3.2.0" % "provided",
+        "org.nd4j" % "nd4j-jcublas-6.0" % "0.0.3.5.5.3-SNAPSHOT" % "provided",
+        "org.nd4j" % "nd4j-api" % "0.0.3.5.5.3-SNAPSHOT" % "provided",
+        "org.deeplearning4j" % "deeplearning4j-nlp" % "0.0.3.3.3.alpha1-SNAPSHOT" % "provided"
           exclude("javax.jms", "jms")
           exclude("com.sun.jdmk", "jmxtools")
           exclude("com.sun.jmx", "jmxri")
